5abdabde4b2f5d64b3203f2d5a76111824fde579
bertvi <bert.viikmae@gofore.com>
2023-02-26 19:43:51 +0200

    Refacto helm charts

--- Docker/securityserver/init_context.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="../../X-Road"
-if [ ! -d "$DIR" ]; then
-  echo "Cloning X-Road source code from Github repository"
-  git clone https://github.com/nordic-institute/X-Road.git $DIR
-fi
-
